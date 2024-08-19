9fd16e1e708b95fd374faccaff21279957aff398
Reynaldo Pena <zonickblack@gmail.com>
2024-08-19 18:01:22 +0000

    Create .wezterm.lua

---
+++ .wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'Seti UI (base16)'
+config.window_background_opacity = 0.9
+
+config.window_decorations = 'NONE'
+
+return config
